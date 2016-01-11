fd57f983b28ea19133303e7d2c1192c4164126d8
nealcrawford <neal@nealcrawford.com>
2016-01-10 17:48:45 -0800

    Clean the code Massive rewrite, hopefully this works. Whats the fun of testing before committing right? Also, only 2 warnings in code analysis now!!

--- src/TwilioTest.java
+++ src/TwilioTest.java
@@ -1,47 +1,39 @@
-// You may want to be more specific in your imports
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
 import com.twilio.sdk.*;
-
-import com.twilio.sdk.resource.instance.*;
-
+import com.twilio.sdk.resource.instance.Message;
 
 public class TwilioTest {
-    // Find your Account Sid and Token at twilio.com/user/account
     public static final String ACCOUNT_SID = "AC3fd9f1b394e4fbcff3966c17c131ef97";
     public static final String AUTH_TOKEN = "5f187afdea5b5b94aaa64d421fb486f7";
-    public static int index;
-    public static ArrayList<String> numDisp;
+    public static ArrayList<String> displacements;
     public static boolean messagesSent;
     public static boolean reconnect;
+    public static int index;
     public static final String FACT_TIME = "15:15"; // 3:15 PM
     public static final String KILLSWITCH_CONFIRM = "Killswitch Activated";
     public static TwilioRestClient client;
     public static MessageSender sender;
-    public static Calendar calendar = Calendar.getInstance();
+    public static Calendar calendar;
 
-    public static void main(String[]args) throws TwilioRestException, IOException {
+    public static void main(String[] args) throws FileNotFoundException {
         client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
         sender = new MessageSender(client);
-        Scanner indexFile = new Scanner(new File("currentIndex.txt"));
-        index = indexFile.nextInt();
-        indexFile.close();
 
+        getDisplacements();
+        getIndex();
+        final ArrayList<String> catFacts = getFacts();
 
-        numDisp = getNumDisplacement();
-
-        // Runs the programLoop once a second
-        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        final ScheduledExecutorService executorService
+                = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(new Runnable() {
-            @Override
             public void run() {
                 try {
-                    programLoop();
+                    eventLoop(catFacts);
                 } catch (TwilioRestException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -51,52 +43,27 @@ public class TwilioTest {
         }, 0, 1, TimeUnit.SECONDS);
     }
 
-    public static void programLoop() throws TwilioRestException, IOException {
-        calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        // Reconnect to twilio every half hour
+    public static void getIndex() throws FileNotFoundException {
+        Scanner indexFile = new Scanner(new File("currentIndex.txt"));
+        index = indexFile.nextInt();
+    }
 
-        reconnect(minute);
-        MessageReader reader = new MessageReader(client);
-        ArrayList<String> newMessages = reader.checkMessages();
-        ArrayList<String> needsMessage = new ArrayList<>();
-        ArrayList<String> msgTxt = new ArrayList<>();
-        for(int i = 0; i < newMessages.size(); i++) {
-            Message message = client.getAccount().getMessage(newMessages.get(i));
-            String from = message.getFrom();
-            if (!from.equals("+18187228329")) {
-                if (killAll(from, message.getBody())) { // Check if message is a killswitch
-                    System.out.println(KILLSWITCH_CONFIRM);
-                    // Confirm that killswitch did work
-                    sender.setPhoneNumber(from);
-                    sender.sendMessage(KILLSWITCH_CONFIRM);
-                    System.exit(0);
-                } else {
-                    if (!subscriber(from)) {   // Check if number is new
-                        addAsSubscriber(from); // Add to subscriber list
-                    }
-                    needsMessage.add(from);
-                    msgTxt.add(message.getBody());
-                }
-            }
+    public static void getDisplacements() throws FileNotFoundException {
+        Scanner numDispFile = new Scanner(new File("numbersDisplacement.txt"));
+        while (numDispFile.hasNextLine()) {
+            displacements.add(numDispFile.nextLine());
         }
+        numDispFile.close();
+    }
 
-        // Send an instant Cat Fact to numbers that request one
-        for(int i = 0; i < needsMessage.size(); i++) {
-            sender.setPhoneNumber(needsMessage.get(i));
-            Scanner indexFile = new Scanner(new File("currentIndex.txt"));
-            int index = indexFile.nextInt();
-            indexFile.close();
-            String sid = sender.sendMessage(getInstantCatFact((""+sender.getPhoneNumber()), index));
-            System.out.println(sid);
-        }
+    public static void eventLoop(ArrayList<String> catFacts) throws TwilioRestException, IOException {
+        String time = getTime();
+        reconnect(time);
+        ArrayList<String> needsFact = parseInbox(getInbox());
+        sendInstantFacts(needsFact, catFacts);
 
-        String time = "" + hour + ":" + minute;
-
-        // If it is 3:00 PM and group fact hasn't been sent, send the group cat fact.
         if (time.equals(FACT_TIME) && !messagesSent) {
-            sendCatFacts(sender);
+            sendGroupFact(sender, catFacts);
             messagesSent = true;
         }
         if (!time.equals(FACT_TIME)) {
@@ -104,117 +71,142 @@ public class TwilioTest {
         }
     }
 
-    public static void reconnect(int minute) {
-        if ((minute == 0 || minute == 30) && !reconnect) {
+    public static String getTime() {
+        calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        return "" + hour + ":" + minute;
+    }
+
+    public static void reconnect(String time) {
+        int minute = Integer.parseInt(time.substring(time.indexOf(":") + 1));
+        if ((minute == 0) || (minute ==30 ) && !reconnect) {
             client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
             sender = new MessageSender(client);
             reconnect = true;
         }
-        if (minute != 0 || minute != 30) {
+        if (minute != 0 && minute != 30) {
             reconnect = false;
         }
     }
 
-    // Add new numbers as subscribers to CatFax
-    public static void addAsSubscriber(String number) throws IOException {
-        // Append numbersDisplacement.txt with the new subscriber's displacement
-        FileOutputStream fileOutStream = new FileOutputStream("numbersDisplacement.txt", true);
-        PrintStream filePrinter = new PrintStream(fileOutStream);
-        filePrinter.println(number);
-        filePrinter.println(index);
-        fileOutStream.close();
-
-        // Update the ArrayList
-        numDisp.add(number);
-        numDisp.add("" + index);
-        System.out.println("Subscriber Added");
-    }
-
-    // Send the timed subscriber cat fact, update the current fact
-    public static void sendCatFacts(MessageSender sender)throws FileNotFoundException, TwilioRestException{
-        getGroupCatFact(index, sender);
-        PrintStream indexWriter = new PrintStream(new File("currentIndex.txt"));
-        indexWriter.print(index + 1);
-        indexWriter.close();
-        System.out.println("sentMessages");
-    }
-
-    // Send the timed cat fact to subscribers
-    public static void getGroupCatFact(int index, MessageSender sender) throws FileNotFoundException, TwilioRestException {
-        Scanner catFaxFile = new Scanner(new File("catFax.txt"));
-        ArrayList<String> catFax = new ArrayList<>();
-        while (catFaxFile.hasNextLine()){
-            catFax.add(catFaxFile.nextLine());
-        }
-
-        // Determine which cat fact to send based on the subscriber's displacement from current fact
-        for (int i = 0; i < numDisp.size(); i++) {
-            String number = numDisp.get(i++);
-            int displacement = Integer.parseInt(numDisp.get(i));
-            // Retrieve catFact
-            int thisIndex = index - displacement;
-            String catFact = catFax.get(thisIndex);
-            sender.setPhoneNumber(number);
-            sender.sendMessage(catFact);
+    public static ArrayList<String> getInbox() {
+        try {
+            MessageReader reader = new MessageReader(client);
+            return reader.checkMessages();
+        } catch (IOException e) {
+            return new ArrayList<>();
         }
     }
 
-    // Return a new and instant cat fact, update the subscriber's displacement
-    public static String getInstantCatFact(String number, int index) throws FileNotFoundException {
-        // Put file into arraylist so we can close the file
-        int displacement = 0; //need to instantiate
-        for(int i = 0; i < numDisp.size(); i += 2){
-            if (numDisp.get(i).equals(number)){
-                displacement = Integer.parseInt(numDisp.get(i+1));
-                numDisp.set((i + 1), "" + (displacement - 1)); //update displacement in ArrayList to be written in file
-                break;
+    public static ArrayList<String> parseInbox(ArrayList<String> inbox) throws TwilioRestException, IOException {
+        ArrayList<String> needsFact = new ArrayList<>();
+        for(String newMessage : inbox) {
+            Message message = client.getAccount().getMessage(newMessage);
+            String from = message.getFrom();
+            if (!from.equals("+18187228329")) {
+                if (isKillswitch(from, message.getBody())) { // Check if message is a killswitch
+                    System.out.println(KILLSWITCH_CONFIRM);
+                    // Confirm that killswitch did work
+                    sender.setPhoneNumber(from);
+                    sender.sendMessage(KILLSWITCH_CONFIRM);
+                    System.exit(0);
+                } else {
+                    if (!isSubscriber(from)) {   // Check if number is new
+                        addSubscriber(from); // Add to subscriber list
+                    }
+                    needsFact.add(from);
+                }
             }
         }
+        return needsFact;
+    }
 
-        // Rewrite printstream
-        PrintStream fileOverwriter = new PrintStream(new File("numbersDisplacement.txt"));
-        for(int i = 0; i < numDisp.size(); i++){
-            fileOverwriter.println(numDisp.get(i));
+    // Return true if the specified numbers text "KILL ALL" to CatFax
+    public static boolean isKillswitch(String number, String message) {
+        if ((number.equals("+13603931867") || number.equals("+13603256564")) && message.equals("KILLALL")) {
+            return true;
+        } else {
+            return false;
         }
-        fileOverwriter.close();
-        // Retrieve catFact
-        index -= displacement;
-        Scanner catFaxFile = new Scanner(new File("catFax.txt"));
-        ArrayList<String> catFax = new ArrayList<>();
-        while(catFaxFile.hasNextLine()) {
-            catFax.add(catFaxFile.nextLine());
-        }
-        String catFact = catFax.get(index % catFax.size());
-        return catFact;
     }
 
     // Check whether the number is currently a subscriber or not
-    public static boolean subscriber(String number) throws FileNotFoundException {
-        for (int i = 0; i < numDisp.size(); i++) {
-            if (number.equals(numDisp.get(i))) {
+    public static boolean isSubscriber(String number) {
+        for (String phoneNumber : displacements) {
+            if (number.equals(phoneNumber)) {
                 return true;
             }
         }
         return false;
     }
 
-    // Return an ArrayList of all numbers and their respective displacements
-    public static ArrayList<String> getNumDisplacement() throws FileNotFoundException {
-        Scanner numDispFile = new Scanner(new File("numbersDisplacement.txt"));
-        ArrayList<String> numDisplacements = new ArrayList<>();
-        while (numDispFile.hasNextLine()) {
-            numDisplacements.add(numDispFile.nextLine());
-        }
-        numDispFile.close();
-        return numDisplacements;
+    // Add new numbers as subscribers to CatFax
+    public static void addSubscriber(String number) throws IOException {
+        // Append numbersDisplacement.txt with the new subscriber's displacement
+        FileOutputStream fileOutStream = new FileOutputStream("numbersDisplacement.txt", true);
+        PrintStream filePrinter = new PrintStream(fileOutStream);
+        filePrinter.println(number);
+        filePrinter.println(index); // Current index is new subscriber's displacement
+        fileOutStream.close();
+
+        // Update the ArrayList
+        displacements.add(number);
+        displacements.add("" + index);
+        System.out.println("Subscriber Added");
     }
 
-    // Return true if the specified numbers text "KILL ALL" to CatFax
-    public static boolean killAll(String number, String message) {
-        if ((number.equals("+13603931867") || number.equals("+13603256564")) && message.equals("KILL ALL")) {
-            return true;
-        } else {
-            return false;
+    public static void sendInstantFacts(ArrayList<String> needsFact, ArrayList<String> catFacts)
+            throws TwilioRestException, FileNotFoundException {
+        for (String phoneNumber : needsFact) {
+            sender.setPhoneNumber(phoneNumber);
+            int displacement = 0; //need to instantiate
+            for(int i = 0; i < displacements.size(); i += 2){
+                if (displacements.get(i).equals(phoneNumber)){
+                    displacement = Integer.parseInt(displacements.get(i+1)); // Get the subscriber's displacement
+                    displacements.set((i + 1), "" + (displacement - 1)); // update their displacement in ArrayList to be written in file
+                    break;
+                }
+            }
+
+            // Rewrite printstream
+            PrintStream fileOverwriter = new PrintStream(new File("numbersDisplacement.txt"));
+            for(String info : displacements){
+                fileOverwriter.println(info);
+            }
+            fileOverwriter.close();
+            // Retrieve catFact
+            String fact = catFacts.get((index - displacement) % catFacts.size());
+            String sid = sender.sendMessage(fact);
+            System.out.println(sid);
         }
+    }
+
+    public static ArrayList<String> getFacts() throws FileNotFoundException {
+        Scanner catFaxFile = new Scanner(new File("catFax.txt"));
+        ArrayList<String> catFacts = new ArrayList<>();
+        while (catFaxFile.hasNextLine()) {
+            catFacts.add(catFaxFile.nextLine());
+        }
+        return catFacts;
+    }
+
+    public static void sendGroupFact(MessageSender sender, ArrayList<String> catFacts) throws FileNotFoundException, TwilioRestException {
+        // Determine which cat fact to send based on the subscriber's displacement from current fact
+        for (int i = 0; i < displacements.size(); i++) {
+            String number = displacements.get(i++);
+            int displacement = Integer.parseInt(displacements.get(i));
+            // Retrieve catFact
+            int thisIndex = index - displacement;
+            String fact = catFacts.get(thisIndex);
+            sender.setPhoneNumber(number);
+            sender.sendMessage(fact);
+        }
+
+        PrintStream indexWriter = new PrintStream(new File("currentIndex.txt"));
+        indexWriter.print(index + 1);
+        indexWriter.close();
+        index++;
+        System.out.println("Group fact sent");
     }
 }
