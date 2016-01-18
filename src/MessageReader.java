3664de309a1d40b9bb853b1175136d1f4b034afe
Ryan <ryanf.rowe@gmail.com>
2016-01-17 17:19:52 -0800

    JavaDocs and optimized imports

--- src/MessageReader.java
+++ src/MessageReader.java
@@ -1,28 +1,46 @@
+/********************************************
+ * MessageReader.java
+ * <p/>
+ * Checks for any unhandled messages
+ ********************************************/
+
 // You may want to be more specific in your imports
-import java.util.*;
-import com.twilio.sdk.*;
-import com.twilio.sdk.resource.instance.*;
-import com.twilio.sdk.resource.list.*;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.resource.instance.Message;
+import com.twilio.sdk.resource.list.MessageList;
+
 import java.io.*;
+import java.util.*;
 
 
 public class MessageReader {
-    // Check for any unhandled messages
     TwilioRestClient client;
     Map<String, String> filters;
 
-    // Accepts a Twillio Client and a Map with message filters
-    // Filters: https://www.twilio.com/docs/api/rest/message#list-get-filters
+    /**
+     * Creates a new MessageReader
+     *
+     * @param client    Twillio Client
+     * @param filters   https://www.twilio.com/docs/api/rest/message#list-get-filters
+     */
     public MessageReader(TwilioRestClient client, Map<String, String> filters) {
         this.client = client;
         this.filters = filters;
     }
 
-    // Returns an ArrayList of new messages following criteria passed in constructor
+    /**
+     * Returns an ArrayList of new messages following criteria passed in constructor.
+     *
+     * @return An ArrayList of new filtered messages from handledMessages.txt
+     * @throws IOException  if handledMessages.txt is not found.
+     */
     public List<String> checkMessages() throws IOException {
         MessageList messages = client.getAccount().getMessages(filters);
+
         List<String> newSIDs = new ArrayList<>();
         List<String> handledSIDs = readInHandled();
+
         for (Message message : messages) {
             String sid = message.getSid();
             boolean found = false;
@@ -51,14 +69,23 @@ public class MessageReader {
         return newSIDs;
     }
 
-    // Read in handled messages to an ArrayList
+    /**
+     * Retrieve handled messages from handledMessages.txt.
+     *
+     * @return ArrayList of handled messages.
+     * @throws FileNotFoundException if handledMessages.txt does not exist.
+     */
     public static List<String> readInHandled() throws FileNotFoundException {
         Scanner fileReader = new Scanner(new File("handledMessages.txt"));
+
         List<String> handledSIDs = new ArrayList<>();
+
         while (fileReader.hasNextLine()) {
             handledSIDs.add(fileReader.nextLine());
         }
+
         fileReader.close();
+
         return handledSIDs;
     }
 }
